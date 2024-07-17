86582c50434e8d3f3639e7991356b2513510ba82
mohashrafemt <122985864+mohashrafemt@users.noreply.github.com>
2024-07-17 13:58:41 +0300

    second

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 
 import './App.css'
 import Navbar from './Components/Navbar'
+import Slider from './Components/Slider'
 
 function App() {
 
   return (
     <>
-      <Navbar/>
+      <Slider/>
     </>
   )
 }
